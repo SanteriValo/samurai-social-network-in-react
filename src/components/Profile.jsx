fe53e466ec7f0f852c247cb1c7225cb27ad39520
SanteriValo <valosanteri@gmail.com>
2025-01-23 19:31:52 +0200

    Created components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>Main content
+                <img
+                    src="https://media.licdn.com/dms/image/v2/D4D16AQHvE3xNl5cTWw/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1731583753566?e=1743033600&v=beta&t=tmrCY-eTfjij1iKONTIaZg5o49EBtc5nTswxlnqpZXo"
+                    alt="main content image one"/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                    <div>
+                        Post 3
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
