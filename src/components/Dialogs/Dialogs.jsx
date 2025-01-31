df7f8719590a3ff0bcd343bec9006f0baca560ae
SanteriValo <valosanteri@gmail.com>
2025-01-31 15:05:48 +0200

    Added dialogs page

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import styles from './Dialogs.module.css'
+
+const Dialogs = (props) => {
+    return (
+        <div>
+            <h2>Dialogs</h2>
+        </div>
+    )
+}
+
+export default Dialogs;
