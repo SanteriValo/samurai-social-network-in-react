9e2573676118068308760e3d6ce40898a11be676
SanteriValo <valosanteri@gmail.com>
2025-01-31 17:24:25 +0200

    Implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import Header from "./components/Header/Header.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Profile from "./components/Profile/Profile.jsx";
 import Dialogs from "./components/Dialogs/Dialogs.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
             <div className="app-content">
-                {/*<Profile />*/}
-                <Dialogs/>
+                <Routes>
+                <Route path='/profile' element={<Profile/>}/>
+                <Route path='/dialogs' element={<Dialogs/>}/>
+                </Routes>
             </div>
         </div>
+        </BrowserRouter>
     )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ console.log(styles)
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
-            <div className={styles.item}><a href="">Profile</a></div>
-            <div className={`${styles.item} ${styles.active}`}><a href="">Messages</a></div>
+            <div className={styles.item}><a href='/profile'>Profile</a></div>
+            <div className={`${styles.item} ${styles.active}`}><a href='/dialogs'>Messages</a></div>
             <div className={styles.item}><a href="">News</a></div>
             <div className={styles.item}><a href="">Music</a></div>
             <div className={styles.item}><a href="">Settings</a></div>
