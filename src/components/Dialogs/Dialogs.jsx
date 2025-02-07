e0834ccd49c204688b87c744595f936cccb7a0ec
Santeri Valo <108551887+SanteriValo@users.noreply.github.com>
2025-02-08 00:39:19 +0200

    added grid css to dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,40 +2,36 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            <h2>Music</h2>
-            <div className={styles.dialogs}>
-                <div className={styles.dialogsItems}>
-                    <div className={styles.dialog}>
-                        John
-                    </div>
-                    <div className={styles.dialog}>
-                        Mike
-                    </div>
-                    <div className={styles.dialog}>
-                        Natalie
-                    </div>
-                    <div className={styles.dialog}>
-                        Julie
-                    </div>
-                    <div className={styles.dialog}>
-                        Louis
-                    </div>
-                    <div className={styles.dialog}>
-                        Harvey
-                    </div>
+        <div className={styles.dialogs}>
+            <h2 className={styles.header}>Dialogs</h2>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog}>
+                    John
+                </div>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    Mike
+                </div>
+                <div className={styles.dialog}>
+                    Natalie
+                </div>
+                <div className={styles.dialog}>
+                    Julie
+                </div>
+                <div className={styles.dialog}>
+                    Louis
+                </div>
+                <div className={styles.dialog}>
+                    Harvey
                 </div>
             </div>
             <div className={styles.messages}>
-                <div className={styles.message}><p>Hi! How are you?</p></div>
-                <div className={styles.message}><p>Whatsapp?</p></div>
-                <div className={styles.message}><p>Yep</p></div>
-                <div className={styles.message}><p>Not a chance!</p></div>
-                <div></div>
-                <div><p>Hi! How are you?</p></div>
+                <div className={styles.message}>Hi! How are you?</div>
+                <div className={styles.message}>Whatsapp?</div>
+                <div className={styles.message}>Yep</div>
+                <div className={styles.message}>Not a chance!</div>
             </div>
         </div>
-    )
+)
 }
 
 export default Dialogs;
