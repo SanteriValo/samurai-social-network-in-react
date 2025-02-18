40a7a44094232b3104524caa085f205060927f5b
SanteriValo <valosanteri@gmail.com>
2025-02-18 21:15:38 +0200

    Moved dialogs data into an array

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,16 +16,21 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    const dialogsData = [
+        {id: 1, name: "Harvey"}, {id: 2, name: "Mike"}, {id: 3, name: "Jessica"}, {id: 4, name: "Donna"}, {id: 5, name: "Louis"}, {id: 6, name: "Rachel"}
+    ]
+
     return (
         <div className={styles.dialogs}>
             <h3 className={styles.header}>Dialogs</h3>
             <div className={styles.dialogsItems}>
-                <DialogItem name="Harvey" id="1"/>
-                <DialogItem name="Mike" id="2"/>
-                <DialogItem name="Jessica" id="3"/>
-                <DialogItem name="Donna" id="4"/>
-                <DialogItem name="Louis" id="5"/>
-                <DialogItem name="Rachel" id="6"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
             </div>
             <div className={styles.messages}>
                 <Message message="Hi! How are you?"/>
