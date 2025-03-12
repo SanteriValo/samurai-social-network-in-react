a265a740be799f59bfd24b2c196f86d6f166d0f2
SanteriValo <valosanteri@gmail.com>
2025-03-12 17:52:08 +0200

    main page navigate added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Dialogs from "./components/Dialogs/Dialogs.jsx";
 import News from "./components/News/News.jsx";
 import Music from "./components/Music/Music.jsx";
 import Settings from "./components/Settings/Settings.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 
 const App = (props) => {
     return (
@@ -16,15 +16,12 @@ const App = (props) => {
                 <Navbar/>
                 <div className="app-content">
                     <Routes>
-                        <Route path='/profile'
-                               element={<Profile
-                                   state={props.state.profilePage} />}/>
-                        <Route path='/dialogs/*'
-                               element={<Dialogs
-                                   state={props.state.dialogsPage}/>}/>
+                        <Route path='/profile' element={<Profile state={props.state.profilePage} />}/>
+                        <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage}/>}/>
                         <Route path='/news' element={<News/>}/>
                         <Route path='/music' element={<Music/>}/>
                         <Route path='/settings' element={<Settings/>}/>
+                        <Route path='/' element={<Navigate to="/profile" />}/>
                     </Routes>
                 </div>
             </div>
