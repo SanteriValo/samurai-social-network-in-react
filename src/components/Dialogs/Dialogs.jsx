eba2e4a9253cf00c5ff3fbab4738ab6f0f253415
SanteriValo <valosanteri@gmail.com>
2025-04-07 17:46:46 +0300

    feat: handle message creation button and click in dialogs (not wired to state yet)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,16 @@
 import styles from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem.jsx";
 import Message from "./Message/Message.jsx";
+import React from "react";
 
 const Dialogs = (props) => {
     const dialogsElements = props.state.dialogs.map(n => <DialogItem key={n.id} name={n.name} id={n.id}/>)
     const messageElements = props.state.messages.map(m => <Message key={m.id} message={m.message}/>)
+    const newMessageElement = React.createRef();
+
+    const addMessage = () => {
+        alert(newMessageElement.current.value);
+    }
 
     return (
         <div className={styles.dialogs}>
@@ -13,10 +19,12 @@ const Dialogs = (props) => {
                 {dialogsElements}
             </div>
             <div className={styles.messages}>
-            {messageElements}
+                {messageElements}
             </div>
+            <textarea name="" ref={newMessageElement} cols="30" rows="4"></textarea>
+            <button onClick={addMessage} className={styles.createMessageButton}>Send</button>
         </div>
-)
+    )
 }
 
 export default Dialogs;
